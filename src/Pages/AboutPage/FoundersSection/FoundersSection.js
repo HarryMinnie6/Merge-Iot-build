505a4cc18df26374926d52816af8d422f0a3327b
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-01-26 10:47:49 +0200

    90% complete with about page

--- src/Pages/AboutPage/FoundersSection/FoundersSection.js
+++ src/Pages/AboutPage/FoundersSection/FoundersSection.js
@@ -9,14 +9,24 @@ function FoundersSection() {
 
       <div className="founders__wrapper">
         <div className="founder">
-          <div className="founder__image"></div>
+          <div className="founder__imageHolder">
+            <img
+              className="founder__image"
+              src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            />
+          </div>
           <div className="founder__paragraph">
             <h3>Name Surname</h3>
             <i>dfdk</i>
           </div>
         </div>
         <div className="founder founder2">
-          <div className="founder__image"></div>
+          <div className="founder__imageHolder">
+            <img
+              className="founder__image"
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            />
+          </div>
           <div className="founder__paragraph founder__paragraph2">
             <h3>Name Surname</h3>
             <i>dfdk</i>
